280028d9220fe38821d790e4bb2e310e97564666
brightalot <70755285+brightalot@users.noreply.github.com>
2020-12-18 17:28:41 +0900

    Update tetris_model.py
    12/18

--- tetris_model.py
+++ tetris_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+#게임의 데이터 모델 구현.
 import random
+
 
 class Shape(object):
     shapeNone = 0
@@ -13,21 +14,18 @@ class Shape(object):
     shapeS = 6
     shapeZ = 7
 
-    shapeCoord = ( #좌표설정
-        ((0, 0), (0, 0), (0, 0), (0, 0)),
-        ((0, -1), (0, 0), (0, 1), (0, 2)),
-        ((0, -1), (0, 0), (0, 1), (1, 1)),
-        ((0, -1), (0, 0), (0, 1), (-1, 1)),
-        ((0, -1), (0, 0), (0, 1), (1, 0)),
-        ((0, 0), (0, -1), (1, 0), (1, -1)),
-        ((0, 0), (0, -1), (-1, 0), (1, -1)),
-        ((0, 0), (0, -1), (1, 0), (-1, -1))
-    )
+    shapeCoord = (  #좌표설정
+        ((0, 0), (0, 0), (0, 0), (0, 0)), ((0, -1), (0, 0), (0, 1), (0, 2)),
+        ((0, -1), (0, 0), (0, 1), (1, 1)), ((0, -1), (0, 0), (0, 1), (-1, 1)),
+        ((0, -1), (0, 0), (0, 1),
+         (1, 0)), ((0, 0), (0, -1), (1, 0),
+                   (1, -1)), ((0, 0), (0, -1), (-1, 0),
+                              (1, -1)), ((0, 0), (0, -1), (1, 0), (-1, -1)))
 
-    def __init__(self, shape=0): #변수 초기화
+    def __init__(self, shape=0):  #변수 초기화
         self.shape = shape
 
-    def getRotatedOffsets(self, direction): #회전시키기
+    def getRotatedOffsets(self, direction):  #회전시키기
         tmpCoords = Shape.shapeCoord[self.shape]
         #조건에 따라 coords 값 리턴
         if direction == 0 or self.shape == Shape.shapeO:
@@ -49,7 +47,8 @@ class Shape(object):
                 return ((y, -x) for x, y in tmpCoords)
 
     def getCoords(self, direction, x, y):
-        return ((x + xx, y + yy) for xx, yy in self.getRotatedOffsets(direction))
+        return ((x + xx, y + yy)
+                for xx, yy in self.getRotatedOffsets(direction))
 
     def getBoundingOffsets(self, direction):
         tmpCoords = self.getRotatedOffsets(direction)
@@ -66,63 +65,73 @@ class Shape(object):
         return (minX, maxX, minY, maxY)
 
 
-class BoardData(object):
-    width = 10
-    height = 22
+class BoardData(object):  #보드 정보
+    width = 10  #가로
+    height = 22  #높이
 
-    def __init__(self):
+    def __init__(self): 
         self.backBoard = [0] * BoardData.width * BoardData.height
 
         self.currentX = -1
         self.currentY = -1
         self.currentDirection = 0
         self.currentShape = Shape()
-        self.nextShape = Shape(random.randint(1, 7))
+        self.nextShape = Shape(random.randint(1, 7))  #다음 블록 모양 
+        #random.randint(1, 7) 1부터 6까지 랜덤 1개.
 
         self.shapeStat = [0] * 8
 
-    def getData(self):
+    def getData(self): #backBoard 데이터 get
         return self.backBoard[:]
 
-    def getValue(self, x, y):
+    def getValue(self, x, y): #백보드 높이?
         return self.backBoard[x + y * BoardData.width]
 
     def getCurrentShapeCoord(self):
-        return self.currentShape.getCoords(self.currentDirection, self.currentX, self.currentY)
+        return self.currentShape.getCoords(self.currentDirection,
+                                           self.currentX, self.currentY)
 
     def createNewPiece(self):
+      #새로운 모양 만들기
         minX, maxX, minY, maxY = self.nextShape.getBoundingOffsets(0)
-        result = False
-        if self.tryMoveCurrent(0, 5, -minY):
+        result = False 
+        #기본 출력값은 False
+
+        #tryMoveCurrent 의 출력값은 boolean값임.
+        if self.tryMoveCurrent(0, 5, -minY): 
+            #참인경우. 즉,  tryMoveCurrent -> tryMove함수 출력시 결과값 True
             self.currentX = 5
             self.currentY = -minY
             self.currentDirection = 0
             self.currentShape = self.nextShape
             self.nextShape = Shape(random.randint(1, 7))
-            result = True
+            result = True #움직인다.
         else:
+          #나머지 경우 즉, False
             self.currentShape = Shape()
             self.currentX = -1
             self.currentY = -1
             self.currentDirection = 0
-            result = False
+            result = False #못움직임.
         self.shapeStat[self.currentShape.shape] += 1
         return result
 
     def tryMoveCurrent(self, direction, x, y):
-        return self.tryMove(self.currentShape, direction, x, y)
+        return self.tryMove(self.currentShape, direction, x, y) # True or False return.
 
-    def tryMove(self, shape, direction, x, y):
+    def tryMove(self, shape, direction, x, y): # 움직이기 가능한가? 판단.
         for x, y in shape.getCoords(direction, x, y):
             if x >= BoardData.width or x < 0 or y >= BoardData.height or y < 0:
-                return False
-            if self.backBoard[x + y * BoardData.width] > 0:
-                return False
-        return True
+                return False #불가능합니다.
+            if self.backBoard[x + y * BoardData.width] > 0: #설정값보다 보드의 넓이가 클 때.
+                return False #불가능합니다.
+      
+        return True #가능!
 
-    def moveDown(self):
+    def moveDown(self): #아래로 움직일게요
         lines = 0
-        if self.tryMoveCurrent(self.currentDirection, self.currentX, self.currentY + 1):
+        if self.tryMoveCurrent(self.currentDirection, self.currentX,
+                               self.currentY + 1):
             self.currentY += 1
         else:
             self.mergePiece()
@@ -131,7 +140,8 @@ class BoardData(object):
         return lines
 
     def dropDown(self):
-        while self.tryMoveCurrent(self.currentDirection, self.currentX, self.currentY + 1):
+        while self.tryMoveCurrent(self.currentDirection, self.currentX,
+                                  self.currentY + 1):
             self.currentY += 1
         self.mergePiece()
         lines = self.removeFullLines()
@@ -139,32 +149,40 @@ class BoardData(object):
         return lines
 
     def moveLeft(self):
-        if self.tryMoveCurrent(self.currentDirection, self.currentX - 1, self.currentY):
+        if self.tryMoveCurrent(self.currentDirection, self.currentX - 1,
+                               self.currentY):
             self.currentX -= 1
 
     def moveRight(self):
-        if self.tryMoveCurrent(self.currentDirection, self.currentX + 1, self.currentY):
+        if self.tryMoveCurrent(self.currentDirection, self.currentX + 1,
+                               self.currentY):
             self.currentX += 1
 
     def rotateRight(self):
-        if self.tryMoveCurrent((self.currentDirection + 1) % 4, self.currentX, self.currentY):
+        if self.tryMoveCurrent((self.currentDirection + 1) % 4, self.currentX,
+                               self.currentY):
             self.currentDirection += 1
             self.currentDirection %= 4
 
     def rotateLeft(self):
-        if self.tryMoveCurrent((self.currentDirection - 1) % 4, self.currentX, self.currentY):
+        if self.tryMoveCurrent((self.currentDirection - 1) % 4, self.currentX,
+                               self.currentY):
             self.currentDirection -= 1
             self.currentDirection %= 4
 
-    def removeFullLines(self):
-        newBackBoard = [0] * BoardData.width * BoardData.height
-        newY = BoardData.height - 1
+    def removeFullLines(self): # 꽉찬 줄 블록 제거하는 함수
+        newBackBoard = [0] * BoardData.width * BoardData.height #새로운 백보드
+        newY = BoardData.height - 1 #새로운 높이
         lines = 0
         for y in range(BoardData.height - 1, -1, -1):
-            blockCount = sum([1 if self.backBoard[x + y * BoardData.width] > 0 else 0 for x in range(BoardData.width)])
+            blockCount = sum([
+                1 if self.backBoard[x + y * BoardData.width] > 0 else 0
+                for x in range(BoardData.width)
+            ])
             if blockCount < BoardData.width:
                 for x in range(BoardData.width):
-                    newBackBoard[x + newY * BoardData.width] = self.backBoard[x + y * BoardData.width]
+                    newBackBoard[x + newY * BoardData.width] = self.backBoard[
+                        x + y * BoardData.width]
                 newY -= 1
             else:
                 lines += 1
@@ -173,7 +191,8 @@ class BoardData(object):
         return lines
 
     def mergePiece(self):
-        for x, y in self.currentShape.getCoords(self.currentDirection, self.currentX, self.currentY):
+        for x, y in self.currentShape.getCoords(self.currentDirection,
+                                                self.currentX, self.currentY):
             self.backBoard[x + y * BoardData.width] = self.currentShape.shape
 
         self.currentX = -1
